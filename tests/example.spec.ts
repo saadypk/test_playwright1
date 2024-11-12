272fadd9f3ce9482f4e52aaa6f7a417739929c4f
saadypk <saadypk@gmail.com>
2024-11-12 17:12:38 +0100

    test_env_ver

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
+  console.log("The value is: "+process.env.PASSWORD);
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
@@ -14,5 +15,5 @@ test('get started link', async ({ page }) => {
   await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'uuu' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
