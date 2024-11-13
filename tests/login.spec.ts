cb7b7126e5849e96f7ebc7730aed768f81c9df24
saadypk <saadypk@gmail.com>
2024-11-13 22:12:09 +0100

    Update login.spec.ts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,12 +2,17 @@ import {expect, test } from '@playwright/test';
 import { LoginPage } from '../pages/loginpage';
 import { StorePage } from '../pages/storepage';
 
+let password: string
 test('Login with Markus', async ({ page }) => {
     const loginPage = new LoginPage(page)
     const storePage = new StorePage(page)
     
+    if(process.env.PASSWORD !== undefined){
+        password = process.env.PASSWORD
+    }
+
     await page.goto("http://hoff.is/login")
-    await loginPage.login("Markus", "sup3rs3cr3t", 'consumer');
+    await loginPage.login("Markus", password, 'consumer');
     const header = await storePage.header.textContent()
 
     expect(header).toBe("Store")
@@ -18,7 +23,7 @@ test('fail login', async ({ page }) => {
     const loginPage = new LoginPage(page);
 
     await page.goto("http://hoff.is/login")
-    await loginPage.login("Markus", "sup3rs3cr3", 'consumer');
+    await loginPage.login("Markus", password, 'consumer');
     const errorMessage = await loginPage.errorMessage.textContent()
 
     expect(errorMessage).toBe("Incorrect password")
