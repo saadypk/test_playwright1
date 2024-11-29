93a3f10c6e515d1db5f14c96e09be2ab551ac8a2
saadypk <saadypk@gmail.com>
2024-11-29 11:31:03 +0100

    removed extra code

--- tests/setup.setup.ts
+++ tests/setup.setup.ts
@@ -1,22 +1,3 @@
-/*import { request, test as setup } from '@playwright/test'
-import { LoginPage } from '../pages/loginpage'
- 
-
-const authFile = 'auth/cookiesState.json'
-setup('Clean up database', async ({page}) => {
-    const apiContext = await request.newContext({
-        baseURL: 'https://test-379574553568.us-central1.run.app',
-        extraHTTPHeaders: {
-          'api_key': 'saad_test',
-          'content-type': 'application/json',
-        },
-    });
-    await apiContext.delete('/student_delete_all')
-    await page.goto('https://hoff.is/cookies/')
-    await new LoginPage(page).login("Saad", "sup3rs3cr3t", 'business')
-    await page.context().storageState({path: authFile})
-
-})*/
 import { request, test as setup } from '@playwright/test';
 import { LoginPage } from '../pages/loginpage';
 
